d1dd3c51ae1534a9c7a14b091e2d4b6fc3c7a48f
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-04-16 02:52:48 +0200

    Update Downloader.ts
    added color to messages
    now using formatted strings
    changed messages / content displayed in console during execution

--- network/Downloader.ts
+++ network/Downloader.ts
@@ -1,6 +1,7 @@
 import fs = require('fs');
 import readline = require('readline');
 import request = require('request');
+import { Printer as P, Printer }  from '../bot/Printer';
 
 export class Downloader
 {
@@ -8,12 +9,13 @@ export class Downloader
 
     public async download(urls: Array<string>): Promise<string>
     {
-        console.log("received " + urls.length + " urls");
+        console.log(P.normal(`received  ${ urls.length } urls`));
         let names = new Array<string>();
-        urls.forEach(url =>
+        for (let i = 0; i < urls.length; i++)
         {
-            names.push(Downloader.getFileName(url));
-        });
+            //names.push(`${i}_${Downloader.getFileName(urls[i])}`);
+            names.push(Downloader.getFileName(urls[i]));
+        }
         names = this.renameFiles(names);
         for (let i = 0; i < urls.length; i++)
         {
@@ -22,19 +24,19 @@ export class Downloader
                 readline.moveCursor(process.stdout, 0, -1);
                 readline.clearLine(process.stdout, 0);
             }
-            process.stdout.write("<<< " + names[i] + "\n");
             let path = this.path + names[i];
             let file = await fs.createWriteStream(path);
+            process.stdout.write(`${this.path} <<< ${this.printName(names[i])}\n`);
             let req = await request.get(urls[i]);
             req.on("response", (response) =>
             {
-                if (response.statusCode !== 200) console.error("status code not 200");
+                if (response.statusCode !== 200) console.error(P.error(`status code is ${response.statusCode}`));
             });
 
             req.on("error", err =>
             {
-                console.error(names[i] + " -> " + err);
-                fs.appendFileSync("url.txt", names[i] + " -> error ");
+                console.error(`${P.error(names[i])} -> ${err})`);
+                fs.appendFileSync(`${this.path}/logs.txt`, names[i] + " -> error ");
                 fs.unlinkSync(path);
             });
 
@@ -52,15 +54,15 @@ export class Downloader
         }
         urls.forEach(url =>
         {
-            fs.appendFileSync("urls.txt", url + "\n");
+            fs.appendFileSync(this.path + "logs.txt", `${url}\n`);
         });
-        console.log("wrote urls into \"urls.txt\"");
+        console.log("wrote results into" + P.info(this.path + "/urls.txt"));
         return "completed";
     }
 
     private renameFiles(names: Array<string>): Array<string>
     {
-        let n_wins: number = 0; // n-wins -> twins = 2, n_wins = n
+        let nuples: number = 0;
         let map = new Map<string, boolean>();
         for (let i = 0; i < names.length; i++)
         {
@@ -71,7 +73,7 @@ export class Downloader
             else
             {
                 let old_name = names[i];
-                if (n_wins > 0)
+                if (nuples > 0)
                 {
                     readline.moveCursor(process.stdout, 0, -2);
                     readline.clearLine(process.stdout, 0);
@@ -79,31 +81,43 @@ export class Downloader
                     readline.clearLine(process.stdout, 0);
                     readline.moveCursor(process.stdout, 0, -1);
                 }
-                process.stdout.write("colliding name : " + old_name.split(".")[0] + "\n");
+                process.stdout.write("colliding names : " + P.warn(nuples + 1) + "\n");
                 let current_name = old_name.split(".")[0];
                 let ext = "." + old_name.split(".")[1];
                 let n: number = 1;
                 while (map.get(current_name + ext)) // change the name
                 {
                     let temp_name = current_name;
-                    temp_name += n;
+                    temp_name += `(${n})`;
                     if (!map.get(temp_name + ext))
                         current_name = temp_name;
                     ++n;
                 }
-                process.stdout.write(old_name + " >>> " + current_name + " (extension " + ext + ")" + "\n");
+                process.stdout.write(P.normal(old_name + P.info(" >>> ") + current_name + " (extension " + ext + ")") + "\n");
                 map.set(current_name + ext, true);
-                ++n_wins;
+                ++nuples;
             }
         }
-        console.log("\nfound " + n_wins + " files with the same name");
+        console.log(P.warn("\nfound " + nuples + " files with the same name"));
         let array = new Array<string>();
         map.forEach((v, k) =>
         {
             array.push(k);
         });
-        if (n_wins > 0) console.log("all twin files renamed to prevent overwriting");
+        if (nuples > 0) console.log(P.info("\tfiles renamed to prevent overwriting"));
         return array;
+    }
+
+    private printName(name: string): string
+    {
+        let res: string;
+        if (name.length > 10)
+        {
+            res = name.substr(0, 10);
+            res += "[...]." + name.split(".")[name.split(".").length - 1];
+        }
+        else res = name;
+        return res;
     }
 
     public get path(): string
