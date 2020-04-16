b23606c14aa2220b27f48feca6020c4df25e84bb
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-04-16 02:50:24 +0200

    Update Bot.ts
    added color to messages
    reworked how the bot select messages to download them later
    added "no talking to strangers" restriction

--- bot/Bot.ts
+++ bot/Bot.ts
@@ -3,13 +3,13 @@ import fs = require('fs');
 import readline = require('readline');
 import { TokenReader, EmojiReader } from './Readers';
 import { Downloader } from '../network/Downloader';
-import { stdout } from 'process';
+import { Printer } from './Printer';
 
 export class Bot 
 {
     // own
     private prefix: string = "/";
-    private readonly parents = ["psyKomicron@6527", "desmoclublyon#3056", "marquez#5719"];
+    private readonly parents = ["psyKomicron#6527", "desmoclublyon#3056", "marquez#5719"];
     // discord
     private client: Discord.Client = new Discord.Client();
     // external
@@ -22,7 +22,7 @@ export class Bot
 
     private init(): void
     {
-        this.client.on("ready", () => { console.log("ready"); });
+        this.client.on("ready", () => { console.log(Printer.info("--------------------------------------------") + "\n\t\tREADY"); });
         this.client.on("message", (message) => { this.onMessage(message); });
         this.client.login(TokenReader.getToken());
     }
@@ -32,7 +32,7 @@ export class Bot
         let content = message.content;
         let channel = message.channel;
         if (!content.startsWith(this.prefix) || !this.parents.includes(this.parseAuthor(message.author))) return;
-        console.log("\ncommand requested by : " + this.parseAuthor(message.author) + "\n");
+        console.log("\ncommand requested by : " + Printer.info(this.parseAuthor(message.author)) + "\n");
         let command = content.split(/ /g);
         switch (command[1])
         {
@@ -41,15 +41,14 @@ export class Bot
                 let limit = pcmd[0];    
                 let type = pcmd[1];
                 message.react(this.emojiReader.getEmoji("thinking"));
-                console.log("Initiating download :");
-                console.log("\tdownloading " + limit);
-                console.log("\tfile type : " + type);
+                console.log("> initiating download :");
+                console.log("\tdownloading " + Printer.info(limit) + "\tfile type : " + Printer.info(type));
                 if (limit > 250)
                 {
-                    console.log("\n\t/!\\ WARNING : downloading over 250 files can fail /!\\ \n");
+                    console.log(Printer.warn("\n\t/!\\ WARNING : downloading over 250 files can fail /!\\ \n"));
                     message.react(this.emojiReader.getEmoji("warning"));
                 }
-                this.initiateDownload(limit, type, channel)
+                this.initiateDownload(limit, channel)
                     .then(() =>
                     {
                         message.react(this.emojiReader.getEmoji("green_check"));
@@ -68,7 +67,37 @@ export class Bot
         }
     }
 
-    private async initiateDownload(numberOfFiles: number, type: FileType, channel: Discord.Channel): Promise<number>
+    /**
+     * /!\ ONLY SUPPORTING PICTURES FOR NOW /!\
+     * Filter the messages looking at their attachements or if they pack a link
+     * @param messages messages to filter
+     * @param type filter for the messages
+     */
+    private filterMessages(messages: Discord.Collection<string, Discord.Message>, type: FileType = FileType.IMG): Discord.Collection<string, Discord.Message>
+    {
+        let filteredArray = new Discord.Collection<string, Discord.Message>();
+        messages.forEach((message, flake) =>
+        {
+            if (message.attachments.size > 0)
+                filteredArray.set(flake, message);
+            else
+            {
+                let content = message.content;
+                if (type == FileType.IMG)
+                {
+                    // will fail when the link is contained in a text -> need to extract it
+                    if (this.isImage(content))
+                    {
+                        message.attachments.forEach(attachement => { attachement.url = content })
+                        filteredArray.set(flake, message);
+                    }
+                }
+            }
+        });
+        return messages.filter(v => v.attachments.size > 0);;
+    }
+
+    private async initiateDownload(numberOfFiles: number, channel: Discord.Channel): Promise<number>
     {
         let downloader = new Downloader();
         let lastMessageID: Discord.Snowflake = null;
@@ -76,36 +105,38 @@ export class Bot
         let totalDownloads: number = 0;
         if (channel instanceof Discord.TextChannel)
         {
-            console.log("downloading files with a " + limit + " file chunk");
+            console.log("downloading files with a " + Printer.info(""+limit) + " chunk");
             let messages = await channel.messages.fetch({ limit: limit });
-            let filteredMessages = messages.filter(v => v.attachments.size > 0);
-            console.log("found " + filteredMessages.size + " messages with attachements...");
-            let urls = this.hydrateUrls(filteredMessages, type);
-            console.log("now have " + urls.length + " matching files");
-            if (urls.length < numberOfFiles) console.log("not enough urls found, searching deeper\n");
-            while (urls.length < numberOfFiles) // fetching all requested urls
+            let filteredMessages = this.filterMessages(messages);//messages.filter(v => v.attachments.size > 0);
+            console.log(Printer.normal("found " + Printer.info(filteredMessages.size) + " messages with attachements..."));
+            let urls = this.hydrateUrls(filteredMessages);
+            console.log("now have " + Printer.info(urls.length) + " matching files");
+            if (urls.length < numberOfFiles)
+                console.log(Printer.normal("not enough urls found, searching deeper\n"));
+            // fetching all requested urls
+            while (urls.length < numberOfFiles) 
             {
                 lastMessageID = await messages.last()?.id;
                 if (lastMessageID == undefined)
                 {
-                    console.log("not more messages to parse, breaking");
+                    console.log(Printer.warn("not more messages to parse, breaking"));
                     break;
                 }
                 else
                 {
                     messages = await channel.messages.fetch({limit: limit, before: lastMessageID });
                     filteredMessages = messages.filter(v => v.attachments.size > 0);
-                    let newUrls = this.hydrateUrls(filteredMessages, type);
+                    let newUrls = this.hydrateUrls(filteredMessages);
                     newUrls.forEach(v => urls.push(v));
 
                     readline.moveCursor(process.stdout, 0, -1);
                     readline.clearLine(process.stdout, 0);
-                    process.stdout.write("\tnow have " + urls.length + " matching files\n");
+                    process.stdout.write("\tnow have " + Printer.info(urls.length) + " matching files\n");
                 }
             }
             let filepath = "./files/" + channel.name;
-            console.log("\nrequest to download " + urls.length + " items in " + filepath);
-            console.log("\t=> user requested " + numberOfFiles);
+            console.log("\nrequest to download " + Printer.info(urls.length) + " items in " + filepath);
+            console.log("\t=> user requested " + Printer.info(numberOfFiles));
             fs.mkdir(filepath, { recursive: true }, (err) => { if (err) throw err; });
             filepath += "/";
             let copyArray: Array<string> = new Array();
@@ -158,7 +189,7 @@ export class Bot
         return [limit, type];
     }
 
-    private hydrateUrls(messages: Discord.Collection<string, Discord.Message>, type: FileType): Array<string>
+    private hydrateUrls(messages: Discord.Collection<string, Discord.Message>, type: FileType = FileType.IMG): Array<string>
     {
         let urls = new Array();
         messages.forEach(message =>
@@ -167,26 +198,10 @@ export class Bot
             {
                 if (type == FileType.IMG) // image files (png, jpg, gif)
                 {
-                    if (Downloader.getFileName(attachement.url).endsWith(".png") ||
-                        Downloader.getFileName(attachement.url).endsWith(".PNG") ||
-                        Downloader.getFileName(attachement.url).endsWith(".jpg") ||
-                        Downloader.getFileName(attachement.url).endsWith(".JPG") ||
-                        Downloader.getFileName(attachement.url).endsWith(".gif") ||
-                        Downloader.getFileName(attachement.url).endsWith(".GIF"))
+                    if (this.isImage(attachement.url))
                     {
                         urls.push(attachement.url);
                     }
-                }
-                if (type == FileType.PDF) // PDF files
-                {
-                    if (Downloader.getFileName(attachement.url).endsWith(".pdf"))
-                    {
-                        urls.push(attachement.url);
-                    }
-                }
-                if (type == FileType.FILE)
-                {
-                    urls.push(attachement.url);
                 }
             });
         });
@@ -199,10 +214,19 @@ export class Bot
         switch (name)
         {
             case "img":
+            case "i":
                 type = FileType.IMG;
                 break;
             case "pdf":
                 type = FileType.PDF;
+                break;
+            case "vid":
+            case "v":
+                type = FileType.VIDEO;
+                break;
+            case "code":
+            case "c":
+                type = FileType.CODE;
                 break;
             default:
                 type = FileType.FILE;
@@ -219,13 +243,25 @@ export class Bot
     {
         let username = author.username;
         let discriminator = author.discriminator;
-        return username + "@" + discriminator;
+        return username + "#" + discriminator;
+    }
+
+    private isImage(content: string)
+    {
+        return (Downloader.getFileName(content).endsWith(".png") ||
+            Downloader.getFileName(content).endsWith(".PNG") ||
+            Downloader.getFileName(content).endsWith(".jpg") ||
+            Downloader.getFileName(content).endsWith(".JPG") ||
+            Downloader.getFileName(content).endsWith(".gif") ||
+            Downloader.getFileName(content).endsWith(".GIF"));
     }
 }
 
 export enum FileType
 {
     PDF = "pdf",
-    IMG = "jpg | png | JPG | PNG",
-    FILE = "file",
+    IMG = "image",
+    VIDEO = "video",
+    CODE = "code",
+    FILE = "all",
 }
