a1cb7f14d7dbefd9369c062d67b302f4abfa966f
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-04-17 01:08:37 +0200

    Update Command.ts
    removed useless interface
    added features to use inheritance

--- bot/commands/Command.ts
+++ bot/commands/Command.ts
@@ -2,10 +2,44 @@ import Discord = require('discord.js');
 
 export abstract class Command
 {
-    abstract async execute(): Promise<CommandResponse>;
-}
+    private name: string;
+    private values: string[]
 
-export interface CommandResponse
-{
-    value: string;
+    protected constructor(name: string, values: string[])
+    {
+        this.name = name;
+        this.values = values;
+    }
+
+    public abstract async execute(): Promise<Object>;
+
+    protected parseMessage(message: Discord.Message): Array<string>
+    {
+        let params = new Array<string>();
+        let strings = message.content.split(" ");
+        let display = "";
+        if (strings.length > 0)
+        {
+            for (let i = 2; i < strings.length; i++)
+            {
+                params.push(strings[i]);
+                display += strings[i];
+            }
+        }
+        return params;
+    }
+
+    public get Name(): string
+    {
+        return this.name;
+    }
+
+    public get Values(): string[]
+    {
+        return this.values;
+    }
+    public set Values(values: string[])
+    {
+        this.values = values;
+    }
 }
