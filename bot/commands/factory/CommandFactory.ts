9581d4bad6fdd5fb034fc58382ce90e70532ccfa
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-04-17 01:09:50 +0200

    Create CommandFactory.ts

---
+++ bot/commands/factory/CommandFactory.ts
@@ -0,0 +1,31 @@
+import Discord = require('discord.js');
+import { Command } from "../Command";
+import { DownloadCommand } from '../DownloadCommand';
+import { DeleteCommand } from '../DeleteCommand';
+import { DefaultCommand } from '../DefaultCommand';
+import { HelpCommand } from './HelpCommand';
+
+export class CommandFactory
+{
+    public static create(type: string, message: Discord.Message): Command
+    {
+        let command: Command = undefined;
+        console.log(type);
+        switch (type)
+        {
+            case "download":
+                command = new DownloadCommand(message);
+                break;
+            case "delete":
+                command = new DeleteCommand(message);
+                break;
+            case "help":
+                command = new HelpCommand(message);
+                break;
+            default:
+                command = new DefaultCommand(message);
+                break;
+        }
+        return command;
+    }
+}
