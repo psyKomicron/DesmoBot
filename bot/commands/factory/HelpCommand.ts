81b999097ee776d3f2cf1c78d4c4b6221b5f5cee
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-04-17 16:50:38 +0200

    now using getters for embed fields values
    little bit easier to read now :)

--- bot/commands/factory/HelpCommand.ts
+++ bot/commands/factory/HelpCommand.ts
@@ -17,19 +17,32 @@ export class HelpCommand extends Command
             .setTitle("Help")
             .setColor(0xff0000)
             .setDescription("Help page for Julie");
-        embed.addField("Explanations", "For commands examples, DO NOT INCLUDE \"[]\" when typing the commands");
-        embed.addField("Download - /download",
-            `/ download [number of files to download] [type of file] [channel id]
-All fields a optional, default values are :
- - number of files to download : 50
- - type of file : images
- - channel id : where the message has been sent`);
-        embed.addField("Delete - /delete", `/ delete [number of messages to delete] [channel id]
-All fields a optional, default values are :
- - number of messages to delete : 10
- - channel id : where the message has been sent`);
+        embed.addField("Explanations", "For commands examples, **DO NOT INCLUDE \"[]\"** when typing the commands");
+        embed.addField("Download - /download", this.downloadHelp);
+        embed.addField("Delete - /delete", this.deleteHelp);
         this.message.channel.send(embed);
         return "executed";
     }
 
+    private get voteHelp(): string
+    {
+
+    }
+
+    private get downloadHelp(): string
+    {
+        return `\`/ download [number of files to download] [type of file] [channel id]\`
+All fields a optional, default values are :
+ - number of files to download : 50
+ - type of file : images
+ - channel id : where the message has been sent`;
+    }
+
+    private get deleteHelp(): string
+    {
+        return `\`/ delete [number of messages to delete] [channel id]\`
+All fields a optional, default values are :
+ - number of messages to delete : 10
+ - channel id : where the message has been sent`;
+    }
 }
