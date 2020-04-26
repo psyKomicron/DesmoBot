32ecc7fc9f52959e9269e8cf61bad3aced38a505
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-04-27 01:29:18 +0200

    Deactivated cursor

--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
 import { Bot } from "./bot/Bot";
-import { Printer } from "./bot/Printer";
+import { Printer } from "./bot/ui/Printer";
 import readline = require('readline');
+import { ProgressBar } from "./bot/ui/ProgressBar";
+import { exit } from "process";
 
-// -------- Code --------
 process.stdout.write("\u001B[?25l");
-
+// -------- Code --------
 console.log(Printer.error("-------------------------------------"));
 console.log(`${Printer.error(">>>>>")} Desmo Bot with TypeScript ${Printer.error("<<<<<")}`); // 59
 process.stdout.write(
