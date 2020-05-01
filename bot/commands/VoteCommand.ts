c8fb127c38473e5cd3b67cccc238e76fb729ab36
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-05-01 22:52:52 +0200

    now working
    - vote system is now working with `collect` & `remove` events correctly
    - the bot is now sending a Embed instead of a simple TextMessage

--- bot/commands/VoteCommand.ts
+++ bot/commands/VoteCommand.ts
@@ -1,106 +1,114 @@
 import Discord = require('discord.js');
 import readline = require('readline');
 import { Command } from './Command';
-import { Printer } from '../ui/Printer';
 import { EmojiReader } from '../Readers';
+import { Printer } from '../../ui/Printer';
 
 export class VoteCommand extends Command
 {
-    // discord
     private message: Discord.Message;
-    private voteChannel: Discord.Channel;
     private collector: Discord.ReactionCollector;
-    // own
-    private users: Map<Discord.User, boolean> = new Map();
-    // arguments
-    private maxVotes: number;
-    private votes: number = 0;
-    private reason: string;
+    private votes: Map<Discord.User, string> = new Map();
+    private emojis: Map<string, boolean> = new Map();
+    private voteValues: [number, string, Discord.Channel];
 
     public constructor(message: Discord.Message)
     {
-        super("vote", ["timeout : 5h"]);
+        super("vote");
         this.message = message;
-        let values = this.getParams(this.parseMessage(this.message));
-        this.maxVotes = values[0];
-        this.reason = values[1];
-        this.voteChannel = values[2];
+        this.voteValues = this.getParams(this.parseMessage(message));
+        let emojiReader = new EmojiReader();
+        this.emojis.set(emojiReader.getEmoji("green_check"), true);
+        this.emojis.set(emojiReader.getEmoji("green_cross"), true);
     }
 
     public async execute(): Promise<Object> 
     {
+        if (this.message.deletable)
+            this.message.delete({ timeout: 100 });
         console.log(Printer.title("starting vote"));
-        console.log(Printer.args(["max votes", "vote reason", "vote channel"], [`${this.maxVotes}`, this.reason, this.voteChannel.id]))
-        if (this.voteChannel instanceof Discord.TextChannel)
+        console.log(Printer.args(["timeout", "vote reason", "vote channel id"], [`${this.voteValues[0]}`, this.voteValues[1], this.voteValues[2].id]))
+        if (this.voteValues[2] instanceof Discord.TextChannel)
         {
-            this.voteChannel.send(this.reason)
-                .then(message =>
-                {
-                    this.buildCollector(message);
-                });
+            this.buildCollector();
         }
         else return "error";
     }
 
-    private buildCollector(message: Discord.Message): void
+    private async buildCollector(): Promise<void>
     {
-        let emojiReader = new EmojiReader();
-        let yes = emojiReader.getEmoji("green_check");
+        // build embed one day
+        let embed = new Discord.MessageEmbed();
+        embed.setTitle(this.voteValues[1]);
+        embed.addField("Temps de vote ", this.voteValues[0] + " secondes", true);
+        let voteMessage = await (this.voteValues[2] as Discord.TextChannel)?.send(embed);
 
-        this.collector = message.createReactionCollector((reaction, user) =>
+        this.collector = voteMessage.createReactionCollector(
+            (reaction: Discord.MessageReaction, user: Discord.User) => !user.bot,
+            {
+                time: this.voteValues[0] * 1000,
+                dispose: true
+            });
+        this.collector.on('collect', (reaction: Discord.MessageReaction, user: Discord.User) =>
         {
-            return reaction.emoji.name == yes || !user.bot;
+            let emojiName = reaction.emoji.name;
+            if (this.emojis.get(emojiName))
+            {
+                this.votes.set(user, emojiName);
+            }
         });
-
-        this.collector.addListener("remove", (reaction, user) =>
+        this.collector.on('remove', (reaction: Discord.MessageReaction, user: Discord.User) =>
         {
-            console.log(`! remove ${reaction.emoji.name}`);
-            console.log(`- ${user.username} removed ${reaction.emoji.name}`);
+            let emojiName = reaction.emoji.name;
+            if (this.votes.has(user) && this.emojis.get(emojiName)) // has voted and removed emoji is a vote emoji
+            {
+                this.votes.delete(user);
+            }
         });
-
-        this.collector.on("collect", (reaction, user) =>
+        this.collector.on('end', (collected: Discord.Collection<string, Discord.MessageReaction>, reason: string) =>
         {
-            console.log(`! collected ${reaction.emoji.name}`);
-            console.log(`- ${user.username} reacted with ${reaction.emoji.name}`);
+            console.log(`Reaction collector ended, info : ${Printer.info(reason)}`);
+            console.log(`collected ${collected.size} reactions, with ${this.votes.size} valid reactions`);
+            embed.addField("DING DING DING !", "Le vote est fini", true);
         });
-
-        this.collector.on("dispose", (reaction, user) =>
-        {
-            console.log(`! dispose ${reaction.emoji.name}`);
-            console.log(`- ${user.username} reacted with ${reaction.emoji.name}`);
-        });
-
-        this.collector.on("end", (collected) =>
-        {
-            console.log(`! vote ended`);
-        });
+        voteMessage.react(new EmojiReader().getEmoji("green_check"))
+            .catch(console.error);
     }
 
-    private parseVote(vote: Discord.MessageReaction): boolean
+    private getParams(map: Map<string, string>): [number, string, Discord.Channel]
     {
-        return false;
-    }
-
-    private getParams(args: string[]): [number, string, Discord.Channel]
-    {
-        let maxVotes: number = 2;
+        let timeout: number = 60;
         let reason: string = "Yes/No";
         let channel: Discord.Channel = this.message.channel;
-        for (let arg in args)
+        map.forEach((value, key) =>
         {
-            if (this.message.guild.channels.resolve(arg) != undefined)
+            switch (key)
             {
-                channel = this.message.guild.channels.resolve(arg);
+                case "r":
+                    if (value != "")
+                        reason = value;
+                    break;
+                case "n":
+                    if (!Number.isNaN(Number.parseInt(value)))
+                        timeout = Number.parseInt(value);
+                    break;
+                case "c":
+                    let resolvedChannel = this.message.guild.channels.resolve(value);
+                    if (resolvedChannel && resolvedChannel instanceof Discord.TextChannel)
+                        channel = resolvedChannel;
+                    break;
+                default:
             }
-            else if (!Number.isNaN(Number.parseInt(arg)))
-            {
-                maxVotes = Number.parseInt(arg);
-            }
-            else if (arg != "")
-            {
-                reason = arg;
-            }
-        }
-        return [maxVotes, reason, channel]
+        });
+        return [timeout, reason, channel]
+    }
+
+    private buildEmbed(title: string, content: string = ""): Discord.MessageEmbed
+    {
+        let embed = new Discord.MessageEmbed()
+            .setTitle(title)
+            .setFooter("Vote requested by " + this.message.author.username);
+        if ()
+        return embed;
     }
 }
