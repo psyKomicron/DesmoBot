c5329cf62d10fb53cb62e33ce01fc4800a957707
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-05-01 22:46:41 +0200

    major changes
    - changed command structure, now looks like a linux command with arguments (-x ... -y ...)
    - added a json log system

--- bot/commands/Command.ts
+++ bot/commands/Command.ts
@@ -1,30 +1,75 @@
+import fs = require('fs');
 import Discord = require('discord.js');
 
 export abstract class Command
 {
+    private static _commands: number;
     private name: string;
-    private values: string[]
+    private values: Map<string, string>;
 
-    protected constructor(name: string, values: string[])
+    protected constructor(name: string)
     {
         this.name = name;
-        this.values = values;
     }
 
     public abstract async execute(): Promise<Object>;
 
-    protected parseMessage(message: Discord.Message): Array<string>
+    protected parseMessage(message: Discord.Message): Map<string, string>
     {
-        let params = new Array<string>();
-        let strings = message.content.split(" ");
-        if (strings.length > 0)
+        // parse with args (-x -y...)
+        let rawContent = message.content.substring(1);
+        // remove command name
+        let substr = 0;
+        while (substr < rawContent.length && rawContent[substr] != "-") { substr++; }
+        let content = rawContent.substring(substr);
+        let map = new Map<string, string>();
+        for (let i = 0; i < content.length; i++)
         {
-            for (let i = 2; i < strings.length; i++)
+            if (content[i] == "-")
             {
-                params.push(strings[i]);
+                if (i + 1 != content.length)
+                {
+                    let key = content[i + 1];
+                    let value = "";
+                    let j = i + 3;
+                    if (content[j] == "\"")
+                    {
+                        j++;
+                        let quote = false;
+                        while (j < content.length && !quote)
+                        {
+                            if (content[j] != "\"")
+                            {
+                                value += content[j];
+                            }
+                            else quote = true;
+                            j++;
+                        }
+                        i = j;
+                    }
+                    else
+                    {
+                        let tac = false;
+                        while (j < content.length && !tac)
+                        {
+                            if (content[j] != '-' && content[j] != " ")
+                                value += content[j];
+                            else tac = true;
+                            j++;
+                        }
+                    }
+                    map.set(key, value);
+                }
             }
         }
-        return params;
+        this.values = map;
+        this.writeLogs(map, message);
+        return map;
+    }
+
+    public static get Commands(): number
+    {
+        return this._commands;
     }
 
     public get Name(): string
@@ -32,12 +77,49 @@ export abstract class Command
         return this.name;
     }
 
-    public get Values(): string[]
+    public get Values(): Map<string, string>
     {
         return this.values;
     }
-    public set Values(values: string[])
+    public set Values(values: Map<string, string>)
     {
         this.values = values;
+    }
+
+    private writeLogs(map: Map<string, string>, message: Discord.Message)
+    {
+        let filepath = "./files/logs";
+        fs.mkdir(filepath, { recursive: true }, (err) => { if (err) throw err; });
+        if (!fs.existsSync("./files/logs/commandlogs.json"))           
+        {
+            let root = [];
+            fs.writeFileSync(filepath + "/commandlogs.json", JSON.stringify(root));
+        }
+        var logs = JSON.parse(fs.readFileSync(filepath + "/commandlogs.json").toString());
+        let now = new Date(Date.now());
+        let data = {};
+        map.forEach((value, key) =>
+        {
+            data[key]= value;
+        });
+        var json = {
+            "user": [
+                { "username": message.author.username },
+                { "discriminator": message.author.discriminator }
+            ],
+            "command": [
+                { "name": this.name },
+                { "arguments": data },
+                { "command number": `${Command.Commands}` }
+            ],
+            "date": [
+                { "day": now.getDate() },
+                { "month": now.getMonth() },
+                { "year": now.getFullYear() },
+                { "epoch": Date.now() }
+            ]
+        }
+        logs.push(json);
+        fs.writeFileSync(filepath + "/commandlogs.json", JSON.stringify(logs));
     }
 }
