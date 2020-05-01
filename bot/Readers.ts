3a20b74195f676af56a90a981359c21ec356c1cb
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-05-01 22:51:02 +0200

    added catch clause with condition

--- bot/Readers.ts
+++ bot/Readers.ts
@@ -1,4 +1,5 @@
 import fs = require('fs');
+import { Printer } from '../ui/Printer';
 
 export class TokenReader
 {
@@ -19,7 +20,18 @@ export class EmojiReader
 {
     getEmoji(name: string): string
     {
-        let res = fs.readFileSync("./bot/emojis/" + name, "UTF-8");
+        let res = undefined;
+        try
+        {
+            res = fs.readFileSync("./bot/emojis/" + name, "UTF-8");
+        }
+        catch (error)
+        {
+            if ((error as Error).name == "EONENT")
+                console.log(Printer.error(`Cannot find emoji ${name}, maybe it was deleted or hasn't been created`));
+            else
+                console.error(Printer.error((error as Error).message));
+        }
         return res;
     }
 }
