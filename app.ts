c446bd418dd1420cf3b4e2d8008caf27aa4ad5c4
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-05-04 22:50:14 +0200

    removed tests

--- app.ts
+++ app.ts
@@ -1,39 +1,12 @@
-import fs = require('fs');
+// necessary imports
 import { Bot } from "./bot/Bot";
-import { WebServer } from "./ui/web/WebServer";
 import { StarEffect } from "./ui/effects/StarEffect";
 import { Printer } from "./ui/Printer";
-import { exit } from "process";
-import { Downloader } from './network/Downloader';
-import { BotEmbed } from './ui/discord/BotEmbed';
+// end necessary imports
+import fs = require('fs');
 
 // ------- Tests --------
-/*let json = JSON.parse(fs.readFileSync("./files/commandlogs.json").toString());
-console.log(json[0]["user"]);
-while (true);*/
-let json = {
-    "embed": {
-        "color": 12,
-        "description": "Test embed",
-        "fields": [
-            {
-                "title": "titre de la categorie 1",
-                "description": "contenu de la categorie"
-            },
-            {
-                "title": "titre de la categorie 2",
-                "description": "contenu de la categorie"
-            },
-            {
-                "title": "titre de la categorie 3",
-                "description": "contenu de la categorie"
-            }
-        ],
-        "footer": "made by psyKomicron",
-        "title": "Title"
-    }
-};
-let embed = new BotEmbed(json).build();
+
 // -------- Code --------
 Printer.startUp();
 let loadingEffect = new StarEffect("", [-17, -1]);
