a2b1cb21c55d0bb2bd7b89f4bbdb5aa2309d45f3
Julien Marquet <psyKomicron@users.noreply.github.com>
2020-05-18 17:43:51 +0200

    now using hash map, untested

--- src/bot/commands/commands/vote/VoteLogger.ts
+++ src/bot/commands/commands/vote/VoteLogger.ts
@@ -2,20 +2,27 @@ import { VoteCommand } from "../VoteCommand";
 
 export class VoteLogger
 {
-    private static votes: Array<VoteCommand> = new Array();
+    private static votes: Map<number, VoteCommand> = new Map();
 
     private constructor() { }
 
     public static log(vote: VoteCommand): number
     {
-        return this.votes.push(vote) - 1;
+        let name = vote.title;
+        // hash title to get id
+        let hashAdrr = 0;
+        for (let counter = 0; counter < name.length; counter++)
+        {
+            hashAdrr = name.charCodeAt(counter) + (hashAdrr << 6) + (hashAdrr << 16) - hashAdrr;
+        }
+        this.votes.set(hashAdrr, vote);
+        return hashAdrr;
     }
 
-    public static end(index: number): void
+    public static end(key: number): void
     {
-        if (index < this.votes.length)
-        {
-            this.votes[index].end("user input");
-        }
+        let vote = this.votes.get(key);
+        if (vote) vote.end("user input");
+        else throw new RangeError("Cannot find key in vote map");
     }
 }
