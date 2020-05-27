b238d06265021a32a9225a007a4d0b193984214d
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-05-27 03:48:04 +0200

    Use YoutubeModule to search

--- src/bot/commands/commands/explore/YTExplorer.ts
+++ src/bot/commands/commands/explore/YTExplorer.ts
@@ -1,16 +1,50 @@
 import { Explorer } from "./Explorer";
+import { Printer } from '../../../../console/Printer';
+import { EmbedFactory } from '../../factory/EmbedFactory';
+import { YoutubeModule } from './youtube/YoutubeModule';
+import { TokenReader } from '../../../dal/Readers';
+import { YoutubeAPIKeyError } from "../../../errors/customs/YoutubeAPIKeyError";
 
 export class YTExplorer extends Explorer
 {
-    public async explore(): Promise<void>
+    public async explore(): Promise<void> 
     {
-        /**
-         * Need to use the 'youtube-search' API.
-         * install youtube-search
-         * create a Google API account
-         * check documentation on how to use the API
-         * when done, parsing JSON should be easy
-        **/
-    }
+        try
+        {
+            let ytModule = new YoutubeModule(TokenReader.getYoutubeAPIKey());
+            console.log(Printer.info("Sucessfully created youtube data API client"));
 
+            ytModule.searchVideos(this.keyword, 10, "en")
+                .then(res =>
+                {
+                    let embed = EmbedFactory.build({
+                        title: "Youtube",
+                        color: Math.floor(Math.random() * 16777215),
+                        description: `Youtube search for \`${this.keyword}\``,
+                        footer: "made by Julie"
+                    });
+                    for (var i = 0; i < 10; i++)
+                    {
+                        let item = res.items[i];
+                        let name = "**" + item.title + "**";
+                        let value = item.videoURL;
+                        if (name && value)
+                        {
+                            embed.addField(name, value);
+                        }
+                    }
+                    embed.setURL(res.items[0].videoURL);
+                    this.send(embed);
+                })
+                .catch(console.error);
+        } catch (e)
+        {
+            if (e instanceof YoutubeAPIKeyError)
+            {
+                console.log(Printer.error("Error occured while searching for youtube videos"));
+                console.error(e.message);
+            }
+        }
+    }
+    
 }
