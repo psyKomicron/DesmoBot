575719b3837f0cdb562c87b6f1eb2c3d53e18645
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-05-27 03:48:16 +0200

    Update ExploreCommand.ts

--- src/bot/commands/commands/ExploreCommand.ts
+++ src/bot/commands/commands/ExploreCommand.ts
@@ -1,20 +1,20 @@
-
-import Discord = require('discord.js');
+import { Message, MessageEmbed } from 'discord.js';
 import { Command } from "../Command";
 import { Bot } from "../../Bot";
-import { CommandSyntaxException } from '../../exceptions/customs/CommandSyntaxException';
+import { CommandSyntaxError } from '../../errors/customs/CommandSyntaxError';
 import { Explorer } from './explore/Explorer';
 import { WikiExplorer } from './explore/WikiExplorer';
 import { Printer } from '../../../console/Printer';
+import { YTExplorer } from './explore/YTExplorer';
+import { WrongArgumentError } from '../../errors/customs/WrongArgumentError';
 
 
 export class ExploreCommand extends Command
 {
     private keyword: string;
-    private domain: string;
     private domainName: string;
 
-    public constructor(message: Discord.Message, bot: Bot)
+    public constructor(message: Message, bot: Bot)
     {
         super("explore", message, bot);
         this.getParams(this.parseMessage());
@@ -22,24 +22,30 @@ export class ExploreCommand extends Command
 
     public async execute(): Promise<void>
     {
-        const url = this.urlize(`${this.domain}${this.keyword}`);
-        console.log(Printer.args(["keyword", "domain", "url"], [`${this.keyword}`, `${this.domain}`, `${url}`]));
+        console.log(Printer.title("explorer"));
+        console.log(Printer.args(["keyword", "domain name"], [`${this.keyword}`, `${this.domainName}`]));
         let e: Explorer;
         switch (this.domainName)
         {
-            case "github":
+            case "youtube":
+                e = new YTExplorer(this.keyword, this);
                 break;
             case "wikipedia":
-                e = new WikiExplorer(url, this);
+                e = new WikiExplorer(this.keyword, this);
                 break;
+            default:
+                throw new WrongArgumentError(this);
         }
-        e.explore();
+        e?.explore();
         this.deleteMessage();
     }
 
-    public send(embed: Discord.MessageEmbed): Promise<Discord.Message>
+    /**
+     * Sends a message to the command message's channel
+     * @param embed
+     */
+    public send(embed: MessageEmbed): Promise<Message>
     {
-        embed.setAuthor(this.bot.client.user.username);
         return this.message.channel.send(embed);
     }
 
@@ -53,24 +59,17 @@ export class ExploreCommand extends Command
                 case "keyword":
                     this.keyword = v;
                     break;
-                case "g":
-                case "git":
-                    this.domain = `https://api.github.com/users/`;
-                    this.domainName = "github";
+                case "yt":
+                case "youtube":
+                    this.domainName = "youtube";
                     break;
                 case "w":
                 case "wiki":
-                    this.domain = `https://en.wikipedia.org/wiki/`;
                     this.domainName = "wikipedia";
                     break;
                 default:
-                    throw new CommandSyntaxException(this);
+                    throw new CommandSyntaxError(this);
             }
         });
-    }
-
-    private urlize(url: string): string
-    {
-        return url.replace(/([ ])/g, "_");
     }
 }
