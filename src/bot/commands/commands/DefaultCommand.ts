c7182f9ef6f658b94b09815857d483bdec33a0bd
psyKomicron <julien@cavaweb.net>
2020-05-29 03:40:17 +0200

    print title

--- src/bot/commands/commands/DefaultCommand.ts
+++ src/bot/commands/commands/DefaultCommand.ts
@@ -1,16 +1,18 @@
-import Discord = require('discord.js');
+import { Message } from 'discord.js';
 import { Command } from '../Command';
 import { Bot } from '../../Bot';
+import { Printer } from '../../../console/Printer';
 
 export class DefaultCommand extends Command
 {
-    public constructor(message: Discord.Message, bot: Bot)
+    public constructor(message: Message, bot: Bot)
     {
         super("default", message, bot);
     }
     
     public async execute(): Promise<void> 
     {
+        console.log(Printer.title("default"));
         this.message.reply("unknown command !");
     }
 }
